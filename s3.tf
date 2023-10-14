176bc710690c3e7f0cab89e6e0e242fed491ecd1
Brent Rogers <brentarogers@gmail.com>
2023-10-14 11:02:46 -0400

    typo?

--- s3.tf
+++ s3.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_object" "bucket_resources" {
   for_each = fileset("./s3_resources/", "*")
 
-  bucket = aws_s3_bucket.bucket.name
+  bucket = aws_s3_bucket.bucket.bucket
   key = "resources/${each.value}"
   source = "./s3_resources/${each.value}"
   etag = filemd5("./s3_resources/${each.value}")
