db9ecbee48f573f48eb065d42c3c790d741dc7e7
Brent Rogers <brentarogers@gmail.com>
2023-10-15 09:29:34 -0400

    adding shutdown alarm

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -4,14 +4,14 @@ resource "aws_cloudwatch_log_group" "valheim_log" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "shutdown" {
-  alarm_name = "shutdown_valheim_when_idle"
+  alarm_name          = "shutdown_valheim_when_idle"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods = 4
-  metric_name = "active_players"
-  namespace = "Valheim"
-  statistic = "Maximum"
-  threshold = 1
-  period = 120
-  actions_enabled = true
-  alarm_actions = []
+  evaluation_periods  = 4
+  metric_name         = "active_players"
+  namespace           = "Valheim"
+  statistic           = "Maximum"
+  threshold           = 1
+  period              = 120
+  actions_enabled     = true
+  alarm_actions       = ["arn:aws:swf:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:action/actions/AWS_EC2.${aws_instance.valheim.id}.Stop/1.0"]
 }
